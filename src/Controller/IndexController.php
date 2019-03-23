34e32e9f54c196f38823b8c254044c35c0c9f1c8
KatNezabydka <KatOrif@ukr.net>
2019-03-23 12:20:03 +0200

    overwrite errors page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -21,51 +21,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
 
-
     /**
      * @Route("/", name="index")
      */
-    public function homepage($name)
+    public function homepage()
     {
         return $this->render('article/homepage.html.twig');
 //        return new Response('IT IS WORK!!!');
     }
 
-//    /**
-//     * @Route("/", name="index")
-//     */
-//    public function homepage()
-//    {
-//        return $this->render('article/homepage.html.twig');
-////        return new Response('IT IS WORK!!!');
-//    }
-//
-//
-//    /**
-//     * @Route("/news/{slug}", name="news.show")
-//     */
-//    public function show($slug)
-//    {
-//        $comments = [
-//            'I ate a normal rock once. It did NOT taste like bacon!',
-//            'Woohoo! I\'m going on an all-asteroid diet!',
-//            'I like bacon too! Buy some from my site! bakinsomebacon.com',
-//        ];
-//        return $this->render('article/show.html.twig', [
-//            'title' => ucwords(str_replace('-', ' ', $slug)),
-//            'comments' => $comments,
-//            'slug' => $slug
-//        ]);
-//    }
-//
-//    /**
-//     * @Route("news/{slug}/heart", name="new_toggle_heart", methods={"POST"})
-//     */
-//    public function toggleArticleHeart($slug, LoggerInterface $logger)
-//    {
-//        $logger->info("Article is being hearted");
-//        return new JsonResponse(['hearts' => rand(5, 100)]);
-//    }
+
+    /**
+     * @Route("/news/{slug}", name="news.show")
+     */
+    public function show($slug)
+    {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+            'slug' => $slug
+        ]);
+    }
+
+    /**
+     * @Route("news/{slug}/heart", name="new_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+        $logger->info("Article is being hearted");
+        return new JsonResponse(['hearts' => rand(5, 100)]);
+    }
 
 
 }
