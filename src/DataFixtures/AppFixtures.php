fc17fef22c30b5d0e1f2b32130751f07fe4b5871
KatNezabydka <KatOrif@ukr.net>
2019-03-24 14:25:30 +0200

    autentification && fixtures User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,12 +3,31 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
+    {
+        $this->loadMicropost($manager);
+        $this->loadUsers($manager);
+    }
+
+    public function loadMicropost(ObjectManager $manager)
     {
 
         for ($i = 0; $i < 10; $i++) {
@@ -18,6 +37,18 @@ class AppFixtures extends Fixture
             $manager->persist($microPost);
         }
 
+        $manager->flush();
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('KatNezabydka');
+        $user->setFullname('Kat Nezabydka');
+        $user->setEmail('katorifmmm@gmail.com');
+        $user->setPassword($this->passwordEncoder->encodePassword($user, '111111'));
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
--- src/Security/ExampleVoter.php
+++ src/Security/ExampleVoter.php
@@ -11,7 +11,8 @@ namespace App\Security;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
 
-class ExampleVoter implements VoterInterface
+class ExampleVoter
+//class ExampleVoter implements VoterInterface
 {
 
     /**
